8126a3099ed78fa481d1efb425447ea69bf8a925
vimalsdev <vimals.dev.2019.cse@rajalakshmi.edu.in>
2022-02-28 18:42:14 +0530

    final change

---
+++ Assignment1/app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.assignment1;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView a;
+    Button b1,b2;
+    Typeface currentStyle=Typeface.MONOSPACE;
+    int currentColor=Color.BLUE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        a=  findViewById(R.id.tv1);
+        b1= findViewById(R.id.bt1);
+        b2= findViewById(R.id.bt2);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view) {
+        if(view == b1)
+        {
+            a.setTypeface(currentStyle);
+            Toast.makeText(this,"text style changed",Toast.LENGTH_SHORT).show();
+            if(currentStyle==Typeface.MONOSPACE)
+                currentStyle=Typeface.SANS_SERIF;
+            else
+                currentStyle=Typeface.MONOSPACE;
+
+            System.out.println("---------------"+currentStyle);
+        }
+        if(view == b2){
+            a.setTextColor(currentColor);
+            Toast.makeText(this,"text color changed",Toast.LENGTH_SHORT).show();
+            if(currentColor==Color.BLUE)
+                currentColor=Color.RED;
+            else
+                currentColor=Color.BLUE;
+
+            System.out.println("---------------- "+currentColor);
+        }
+    }
+}
